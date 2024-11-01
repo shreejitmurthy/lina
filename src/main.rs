5ef396f35b28f88f8601d9c686fdda635a4c988f
Shreejit Murthy <botmorph17@gmail.com>
2024-11-02 01:16:07 +1030

    proper user input

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ use calculator::*;
 use std::io;
 use std::io::Write;
 
+use std::process::exit;
+
 fn main() {
     let mut calc = Calculator::new();
     loop {
@@ -21,40 +23,17 @@ fn main() {
     
         if input.trim() == "exit" || input.trim() == "ex" {
             println!("Exiting...");
-            break;
+            exit(0);
         }
     
         match parse_input(input.as_str()) {
             Ok(parsed_command) => {
                 let result = calc.interpret(parsed_command);
-                println!("=> {}", result)
+                println!("{}", result)
             }
             Err(e) => {
                 println!("Error parsing input: {:?}", e);
             }
         }
     }
-    print!("> ");
-    io::stdout().flush().unwrap();
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
-
-    if input.trim() == "exit" || input.trim() == "ex" {
-        println!("Exiting...");
-        // break;
-    }
-
-    match parse_input(input.as_str()) {
-        Ok(parsed_command) => {
-            let result = calc.interpret(parsed_command);
-            println!("=> {}", result)
-        }
-        Err(e) => {
-            println!("Error parsing input: {:?}", e);
-        }
-    }
-
-
 }
